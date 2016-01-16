3414c780f7a5d211548567964c82a2956b639d94
Jakub Czeczótka <jakub.czeczotka@gmail.com>
2016-01-16 20:45:41 +0000

    Add Distance metres, km, kilometres and miles methods

--- src/main/scala/com/czeczotka/conversion/running/Distance.scala
+++ src/main/scala/com/czeczotka/conversion/running/Distance.scala
@@ -13,10 +13,16 @@ object Distance {
   val metres1200 =    Distance(1200)
   val metres1600 =    Distance(1600)
   val metres2000 =    Distance(2000)
+  val k1 =            Distance(1000)
   val k5 =            Distance(5000)
   val k10 =           Distance(10000)
   val miles10 =       Distance(16093)
   val halfMarathon =  Distance(21097)
   val marathon =      Distance(42195)
+
+  def metres(m: Int) = Distance(m)
+  def km(d: Double) = Distance((d * 1000).toInt)
+  def kilometres(d: Double) = km(d)
+  def miles(d: Double) = km(d * 1.60934)
 
 }
