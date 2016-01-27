ccb384648ca8a6d5cc5bed25b4d571ada4b6f536
Jakub Czeczótka <jakub.czeczotka@gmail.com>
2016-01-27 22:39:52 +0000

    Add Time.string2time implicit conversion method

--- src/main/scala/com/czeczotka/conversion/running/Time.scala
+++ src/main/scala/com/czeczotka/conversion/running/Time.scala
@@ -1,5 +1,8 @@
 package com.czeczotka.conversion.running
 
+import scala.language.implicitConversions
+
+
 case class Time(hours: Int = 0, minutes: Int, seconds: Int) {
 
   override def toString = hours match {
@@ -9,4 +12,25 @@ case class Time(hours: Int = 0, minutes: Int, seconds: Int) {
     }
     case _ => "%dh%02dm%02ds".format(hours, minutes, seconds)
   }
+}
+
+
+object Time {
+
+  val min = """(\d{1,2})m""".r
+  val minSec = """(\d{1,2})m(\d{1,2})s""".r
+  val hourMin = """(\d{1,2})h(\d{1,2})m""".r
+  val hourMinSec = """(\d{1,2})h(\d{1,2})m(\d{1,2})s""".r
+
+  implicit def string2time(str: String): Time = str match {
+    case min(minutes) => Time(0, minutes.toInt, 0)
+    case minSec(minutes, seconds) => Time(0, minutes.toInt, seconds.toInt)
+    case hourMin(hours, minutes) => Time(hours.toInt, minutes.toInt, 0)
+    case hourMinSec(hours, minutes, seconds) => Time(hours.toInt, minutes.toInt, seconds.toInt)
+    case _ => exception(str)
+  }
+
+  private def exception(s: String) =
+    throw new IllegalArgumentException(s"Could not convert string '$s' to time. Please provide the time in the 'XXhXXmXXs' format.")
+
 }
--- src/test/scala/com/czeczotka/conversion/running/TimeSpec.scala
+++ src/test/scala/com/czeczotka/conversion/running/TimeSpec.scala
@@ -1,15 +1,37 @@
 package com.czeczotka.conversion.running
 
 import org.specs2.mutable.Specification
+import com.czeczotka.conversion.running.Time.string2time
 
 class TimeSpec extends Specification {
 
   "Time.toString" should {
     "return a String representing the Time" in {
-      Time(1, 2, 3).toString mustEqual "1h02m03s"
-      Time(12, 34, 56).toString mustEqual "12h34m56s"
-      Time(0, 2, 3).toString mustEqual "2m03s"
-      Time(0, 0, 3).toString mustEqual "3s"
+      Time(1, 2, 3).toString      mustEqual "1h02m03s"
+      Time(12, 34, 56).toString   mustEqual "12h34m56s"
+      Time(0, 2, 3).toString      mustEqual "2m03s"
+      Time(0, 0, 3).toString      mustEqual "3s"
+    }
+  }
+
+  "Time.string2time" should {
+    "convert a String to a Time object" in {
+      string2time("5m")         mustEqual Time(0, 5, 0)
+      string2time("5m5s")       mustEqual Time(0, 5, 5)
+      string2time("05m05s")     mustEqual Time(0, 5, 5)
+      string2time("50m50s")     mustEqual Time(0, 50, 50)
+      string2time("2h59m")      mustEqual Time(2, 59, 0)
+      string2time("15h5m50s")   mustEqual Time(15, 5, 50)
+      string2time("0h0m1s")     mustEqual Time(0, 0, 1)
+    }
+
+    "not convert an invalid String" in {
+      def errorMessage(s: String) = s"Could not convert string '$s' to time. Please provide the time in the 'XXhXXmXXs' format."
+      string2time("")              must throwA[IllegalArgumentException](errorMessage(""))
+      string2time("hms")           must throwA[IllegalArgumentException](errorMessage("hms"))
+      string2time("h11m22s")       must throwA[IllegalArgumentException](errorMessage("h11m22s"))
+      string2time("111h222m333s")  must throwA[IllegalArgumentException](errorMessage("111h222m333s"))
+      string2time("hms")           must throwA[IllegalArgumentException](errorMessage("hms"))
     }
   }
 }
