407fac7545a1fb210f07fb8d1699166f0eae32ea
Jakub Czeczótka <jakub.czeczotka@gmail.com>
2016-03-09 23:50:06 +0000

    Add Splits - use named arguments in the spec

--- src/test/scala/com/czeczotka/conversion/running/SplitsSpec.scala
+++ src/test/scala/com/czeczotka/conversion/running/SplitsSpec.scala
@@ -6,12 +6,12 @@ class SplitsSpec extends Specification {
 
   "Splits" should {
     "contain correct values" in {
-      Splits(5.0, 1.0).markers mustEqual List(1.0, 2.0, 3.0, 4.0, 5.0)
-      Splits(4.5, 1.0).markers mustEqual List(1.0, 2.0, 3.0, 4.0, 4.5)
-      Splits(0.9, 1.0).markers mustEqual List(0.9)
-      Splits(1.1, 1.0).markers mustEqual List(1.0, 1.1)
-      Splits(0.5, 0.1).markers mustEqual List(0.1, 0.2, 0.3, 0.4, 0.5)
-      Splits(2.5, 0.4).markers mustEqual List(0.4, 0.8, 1.2, 1.6, 2.0, 2.4, 2.5)
+      Splits(distance = 5.0, step = 1.0).markers mustEqual List(1.0, 2.0, 3.0, 4.0, 5.0)
+      Splits(distance = 4.5, step = 1.0).markers mustEqual List(1.0, 2.0, 3.0, 4.0, 4.5)
+      Splits(distance = 0.9, step = 1.0).markers mustEqual List(0.9)
+      Splits(distance = 1.1, step = 1.0).markers mustEqual List(1.0, 1.1)
+      Splits(distance = 0.5, step = 0.1).markers mustEqual List(0.1, 0.2, 0.3, 0.4, 0.5)
+      Splits(distance = 2.5, step = 0.4).markers mustEqual List(0.4, 0.8, 1.2, 1.6, 2.0, 2.4, 2.5)
     }
   }
 }
