06fbf8e088709e84655bc72713932faa25cba36a
Jakub Czeczótka <jakub.czeczotka@gmail.com>
2016-04-16 23:31:15 +0100

    Add Splits as Doubles

--- src/main/scala/com/czeczotka/conversion/running/Activity.scala
+++ src/main/scala/com/czeczotka/conversion/running/Activity.scala
@@ -1,8 +1,16 @@
 package com.czeczotka.conversion.running
 
+import com.czeczotka.conversion.running.Distance.k1
+
+import scala.collection.immutable.SortedMap
+
 case class Activity(pace: Pace, distance: Distance) {
 
-  def splits: Map[Int, String] = (for (km <- 1 to distance.kilometres.toInt) yield km -> Activity(pace, Distance(km * 1000)).time.toString).toMap
+  def splits(d: Distance): Map[Double, String] = SortedMap[Double, String](
+    (for (i <- Range.Double.inclusive(d.kilometres, distance.kilometres, d.kilometres))
+      yield i -> Activity(pace, Distance((i * 1000).toInt)).time.toString): _*)
+
+  def splits: Map[Double, String] = splits(k1)
 
   def time: Time = {
     val totalSeconds: Int = (distance.metres / 1000.0 * pace.inSeconds).toInt
--- src/test/scala/com/czeczotka/conversion/running/ActivitySpec.scala
+++ src/test/scala/com/czeczotka/conversion/running/ActivitySpec.scala
@@ -3,6 +3,8 @@ package com.czeczotka.conversion.running
 import com.czeczotka.conversion.running.Distance._
 import org.specs2.mutable.Specification
 
+import scala.collection.immutable.SortedMap
+
 class ActivitySpec extends Specification {
 
   "Activity.time" should {
@@ -19,14 +21,18 @@ class ActivitySpec extends Specification {
     }
   }
 
-  "Activity.splits" should {
+  "Activity.splitsDouble" should {
     "create splits for every kilometer" in {
       Activity("5:00", metres600).splits mustEqual Map()
-      Activity("5:00", k10).splits mustEqual Map(1 -> "5m00s",  2 -> "10m00s", 3 -> "15m00s", 4 -> "20m00s", 5 -> "25m00s",
-                                                 6 -> "30m00s", 7 -> "35m00s", 8 -> "40m00s", 9 -> "45m00s", 10 -> "50m00s")
-      Activity("4:45", k5).splits mustEqual  Map(1 -> "4m45s",  2 -> "9m30s",  3 -> "14m15s", 4 -> "19m00s", 5 -> "23m45s")
-      Activity("5:00", Distance(9500)).splits mustEqual Map(1 -> "5m00s",  2 -> "10m00s", 3 -> "15m00s", 4 -> "20m00s", 5 -> "25m00s",
-                                                            6 -> "30m00s", 7 -> "35m00s", 8 -> "40m00s", 9 -> "45m00s")
+      Activity("6:00", metres400).splits(Distance(100)) mustEqual SortedMap(0.1 -> "36s",  0.2 -> "1m12s", 0.3 -> "1m48s", 0.4 -> "2m24s")
+      Activity("4:45", k5).splits  mustEqual SortedMap(1.0 -> "4m45s",  2.0 -> "9m30s",  3.0 -> "14m15s", 4.0 -> "19m00s", 5.0 -> "23m45s")
+      Activity("5:00", k10).splits mustEqual SortedMap(1.0 -> "5m00s",  2.0 -> "10m00s", 3.0 -> "15m00s", 4.0 -> "20m00s", 5.0  -> "25m00s",
+                                                       6.0 -> "30m00s", 7.0 -> "35m00s", 8.0 -> "40m00s", 9.0 -> "45m00s", 10.0 -> "50m00s")
+      Activity("5:00", Distance(9500)).splits mustEqual SortedMap(1.0 -> "5m00s",  2.0 -> "10m00s", 3.0 -> "15m00s", 4.0 -> "20m00s", 5.0 -> "25m00s",
+                                                                  6.0 -> "30m00s", 7.0 -> "35m00s", 8.0 -> "40m00s", 9.0 -> "45m00s")
+      Activity("5:40", marathon).splits(k5) mustEqual SortedMap(5.0  -> "28m20s",   10.0 -> "56m40s",   15.0 -> "1h25m00s", 20.0 -> "1h53m20s",
+                                                                25.0 -> "2h21m40s", 30.0 -> "2h50m00s", 35.0 -> "3h18m20s", 40.0 -> "3h46m40s")
+      marathon in "2h15m25s" splits k10 mustEqual SortedMap(10.0 -> "32m00s", 20.0 -> "1h04m00s", 30.0 -> "1h36m00s", 40.0 -> "2h08m00s")
     }
   }
 }
