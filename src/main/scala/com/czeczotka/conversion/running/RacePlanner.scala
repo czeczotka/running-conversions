018d1d6e60e56c7bc679c31b6326ddf4042ac7cc
Jakub Czeczótka <jakub.czeczotka@gmail.com>
2017-11-03 20:37:45 +0000

    Make RacePlanner include the finish time

--- src/main/scala/com/czeczotka/conversion/running/RacePlanner.scala
+++ src/main/scala/com/czeczotka/conversion/running/RacePlanner.scala
@@ -9,7 +9,7 @@ case class RacePlanner(activity: Activity, timeOfDay: LocalTime) {
   def splits(): Map[Double, (String, String)] = splits(Distance.k1)
 
   def splits(d: Distance): Map[Double, (String, String)] = SortedMap((for {
-      i <- Range.Double.inclusive(d.kilometres, activity.distance.kilometres, d.kilometres)
+      i <- Range.Double.inclusive(d.kilometres, activity.distance.kilometres, d.kilometres) :+ activity.distance.kilometres
     } yield {
       val time = Activity(activity.pace, Distance((i * 1000).toInt)).time
       i -> (time.toString, timeOfDay.plusSeconds(time.toSeconds).toString)
