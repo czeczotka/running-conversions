7e6fdc6437fb068b24f9c969c27616daa9e5865a
Jakub Czeczótka <jakub.czeczotka@gmail.com>
2017-11-05 00:01:46 +0000

    Add RacePlanner prettyPrint

--- src/main/scala/com/czeczotka/conversion/running/RacePlanner.scala
+++ src/main/scala/com/czeczotka/conversion/running/RacePlanner.scala
@@ -6,6 +6,23 @@ import scala.collection.immutable.SortedMap
 
 case class RacePlanner(activity: Activity, timeOfDay: LocalTime) {
 
+  private lazy val header =
+    """*----------------------------------*
+      #|  split | race time | time of day |
+      #|----------------------------------|""".stripMargin('#')
+
+  private lazy val footer = "*----------------------------------*"
+
+  private lazy val body = {
+    val s = splits()
+    val pattern = s"%${s.values.map(_._1).map(_.length).max}s"
+    s.map { s =>
+      s"|   ${s._1}  |   ${pattern.format(s._2._1)}  |    ${s._2._2}    |"
+    }.mkString("\n")
+  }
+
+  lazy val prettyPrint: String = Seq(header, body, footer).mkString("\n")
+
   def splits(): Map[Double, (String, String)] = splits(Distance.k1)
 
   def splits(d: Distance): Map[Double, (String, String)] = SortedMap((for {
